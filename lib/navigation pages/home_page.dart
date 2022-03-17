c437a46589a701cec006d788c97da653659f5608
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2022-03-17 18:42:53 +0530

    Title bar design
    initlized

--- lib/navigation pages/home_page.dart
+++ lib/navigation pages/home_page.dart
@@ -1,3 +1,6 @@
+
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,13 +12,80 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  static const historyLength = 5;
+  List<String> _searchHistory = [
+    'Search history 1',
+    'Search history 2',
+    'Search history 3',
+    'Search history 4',
+    'Search history 5',
+  ];
+  List<String> _searchHistoryFiltered = [];
+  String _selectedTerm = '';
+  
+  List<String> filterSearchTerms({
+  required String filter,
+}) {
+  if (filter != null && filter.isNotEmpty) {
+    // Reversed because we want the last added items to appear first in the UI
+    return _searchHistory.reversed
+        .where((term) => term.startsWith(filter))
+        .toList();
+  } else {
+    return _searchHistory.reversed.toList();
+  }
+}
+
+void addSearchTerm(String term) {
+  if (_searchHistory.contains(term)) {
+    // This method will be implemented soon
+    putSearchTermFirst(term);
+    return;
+  }
+  _searchHistory.add(term);
+  if (_searchHistory.length > historyLength) {
+    _searchHistory.removeRange(0, _searchHistory.length - historyLength);
+  }
+  // Changes in _searchHistory mean that we have to update the filteredSearchHistory
+  _searchHistoryFiltered = filterSearchTerms(filter: '');
+}
+
+void deleteSearchTerm(String term) {
+  _searchHistory.removeWhere((t) => t == term);
+  _searchHistoryFiltered = filterSearchTerms(filter: '');
+}
+
+void putSearchTermFirst(String term) {
+  deleteSearchTerm(term);
+  addSearchTerm(term);
+}
+
+@override
+void initState() {
+  super.initState();
+  _searchHistoryFiltered = filterSearchTerms(filter: '');
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body:
+       Column(
         children: [
           Container(
-            padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
+
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.05),
+                    offset: Offset(0.0, 1.0), //(x,y)
+                    blurRadius: 6.0,
+                  ),]
+            ),
+            padding: const EdgeInsets.only(top: 55, left: 20, right: 20, bottom: 15),
+            
             child: Row(
               children: [
                 Icon(Icons.menu, size: 25, color: Colors.black54,),
