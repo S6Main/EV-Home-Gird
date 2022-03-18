9679ab30e53f198467ec39c4c900e8106b70ca1e
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2022-03-18 18:49:22 +0530

    Google Map
    Google Map failed successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,60 @@
-import 'package:ev_homegrid/icons/custom_icon.dart';
-import 'package:ev_homegrid/temp.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'Guest.dart';
-import 'MetaMask.dart';
-import 'temp.dart';
-import 'navigation pages/main_page.dart';
-import 'navigation pages/pop_pages/side_page.dart';
-void main() {
-  runApp(MyApp());
-}
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'EV Home Grid',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(),
-      home: MainPage(),
-      //home: MyApp()
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
     );
   }
 }
-class MyHomePage extends StatefulWidget {
+
+class MapSample extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MapSample> createState() => MapSampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  //MyHomePage({Key key, this.title}) : super(key: key);
-  //final String title;
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      /*appBar: AppBar(
-        title: Text('EV Home Grid'),
-      ),*/
-      body: Center(
-      child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-      SizedBox(
-        height: 550,
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
-      SizedBox(
-      width: 330,
-      height: 40,
-      child: ElevatedButton.icon(
-          icon: Icon(
-            CustomIcon.account_balance_wallet,
-            size: 24,
-            color: Color.fromARGB(255,66, 165, 245),
-            ),
-          label: Text('METAMASK'),
-            style: ElevatedButton.styleFrom(
-              primary: Color.fromARGB(255, 255, 255, 255),
-              onPrimary: Color.fromARGB(255, 0, 0, 0),
-              shadowColor: Color.fromARGB(68, 0, 0, 0),
-              side: BorderSide(width: 1.5, color: Color.fromARGB(75, 0, 0, 0)),
-              elevation: 1,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              minimumSize: Size(100, 40), //////// HERE
-            ),
-            onPressed: () {},
-          )
-    ),
-    SizedBox(
-      height: 16,
-    ),
-    SizedBox(
-      width: 330,
-      height: 40,
-      child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Colors.black,
-              onPrimary: Colors.white,
-              shadowColor: Color.fromARGB(255, 65, 65, 65),
-              elevation: 2,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)),
-              minimumSize: Size(100, 40), //////// HERE
-            ),
-            onPressed: () {
-              Navigator.push(context,
-              MaterialPageRoute(builder: (context) =>  MainPage()),
-              );
-            },
-            child: Text('GUEST'),
-          )
-      ,),
-    ],)));
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
