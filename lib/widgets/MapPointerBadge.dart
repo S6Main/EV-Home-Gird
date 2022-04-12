b3ac9bf93f8aac5a8a751bfd8839add711a8bfac
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2022-04-12 19:03:46 +0530

    Revert "Popup box created"
    This reverts commit 01d074837ff6c299611d3c79a8c35476ff24199f.

--- lib/widgets/MapPointerBadge.dart
+++ lib/widgets/MapPointerBadge.dart
@@ -1,12 +1,13 @@
-// import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ev_homegrid/globals.dart';
-import '../navigation pages/main_page.dart';
+import  'package:ev_homegrid/globals.dart';
 
 class MapPointerBadge extends StatelessWidget {
-  bool isSelected = false;
 
-  MapPointerBadge({required this.isSelected});
+  bool isSelected  = false;
+
+  MapPointerBadge({ required this.isSelected });
 
   @override
   Widget build(BuildContext context) {
@@ -16,37 +17,30 @@ class MapPointerBadge extends StatelessWidget {
       padding: EdgeInsets.all(12),
       margin: EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
       decoration: BoxDecoration(
-          color: this.isSelected ? Globals.MAIN_COLOR : Colors.white,
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 10,
-              offset: Offset.zero,
-            )
-          ]),
+        color: this.isSelected ? Globals.MAIN_COLOR :Colors.white,
+        borderRadius: BorderRadius.circular(100),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset.zero,
+          )
+        ]
+      ),
       child: Row(
         children: [
-          GestureDetector(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => _buildPopupDialog(context),
-              );
-            },
-            child: Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/avatar.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  border: Border.all(
-                      color:
-                          this.isSelected ? Colors.white : Globals.MAIN_COLOR,
-                      width: 1)),
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              image: DecorationImage(image: AssetImage('assets/images/avatar.png'),
+                 fit: BoxFit.cover
+                 ),
+                 border: Border.all(
+                   color: this.isSelected ? Colors.white :Globals.MAIN_COLOR,
+                   width: 1
+                 )
             ),
           ),
           SizedBox(width: 10),
@@ -54,137 +48,30 @@ class MapPointerBadge extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Roman Jaquez',
+                Text('Roman Jaquez',
                   style: TextStyle(
                     fontWeight: FontWeight.w400,
-                    color: this.isSelected ? Colors.white : Colors.grey,
+                    color: this.isSelected ? Colors.white :Colors.grey,
                   ),
                 ),
-                Text(
-                  'Mi location',
+                Text('Mi location',
                   style: TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.normal,
-                    color: this.isSelected ? Colors.white : Colors.grey,
+                    color: this.isSelected ? Colors.white :Colors.grey,
                   ),
                 ),
               ],
             ),
           ),
           Icon(
-            Icons.search_rounded,
-            color: this.isSelected ? Colors.white : Globals.MAIN_COLOR,
-            size: 40,
-          )
+            Icons.my_location,
+            color: this.isSelected ? Colors.white :Globals.MAIN_COLOR,
+            size: 40,)
+          
         ],
       ),
-    );
+      );
   }
 }
 
-//Popup Dialog Box
-Widget _buildPopupDialog(BuildContext context) {
-  return AlertDialog(
-    shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(32.0))),
-    contentPadding: EdgeInsets.only(top: 10.0),
-    content: Container(
-      width: 300.0,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                "Nick Name :",
-                style: TextStyle(fontSize: 18.0),
-              ),
-              Row(children: <Widget>[
-                Text("Roman Jaquez", style: TextStyle(fontSize: 16.0)),
-              ])
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Divider(
-            color: Colors.grey,
-            height: 4.0,
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text("Wallet Address :", style: TextStyle(fontSize: 18.0)),
-              Row(children: <Widget>[
-                Text("d148aseb", style: TextStyle(fontSize: 16.0)),
-              ])
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Divider(
-            color: Colors.grey,
-            height: 4.0,
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                "Vehicle Type :",
-                style: TextStyle(fontSize: 18.0),
-              ),
-              Row(children: <Widget>[
-                Text("EV", style: TextStyle(fontSize: 16.0)),
-              ])
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          InkWell(
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => MainPage(),
-              );
-            },
-            child: Container(
-              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(32.0),
-                    bottomRight: Radius.circular(32.0)),
-              ),
-              child: Text(
-                "Close",
-                style: TextStyle(color: Colors.white),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
