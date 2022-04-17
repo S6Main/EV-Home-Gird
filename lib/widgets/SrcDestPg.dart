49271237ab986c65f1d10dfe0bb2819658a8644d
JYOTHIS K S <65806949+JYOTHIS10@users.noreply.github.com>
2022-04-17 23:09:37 +0530

    searchBar UI
    Not completed

--- lib/widgets/SrcDestPg.dart
+++ lib/widgets/SrcDestPg.dart
@@ -1,85 +1,76 @@
-// main.dart
+import 'package:ev_homegrid/widgets/search.dart';
 import 'package:flutter/material.dart';
-
-import '../navigation pages/main_page.dart';
 
 class SrcDestpg extends StatelessWidget {
   const SrcDestpg({Key? key}) : super(key: key);
+
+  get child => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 40.0,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    hintText: 'Source',
-                    contentPadding: const EdgeInsets.all(20),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30))),
-                onChanged: (value) {
-                  // do something
-                },
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    hintText: 'Destination',
-                    contentPadding: const EdgeInsets.all(20),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30))),
-                onChanged: (value) {
-                  // do something
-                },
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              SizedBox(
-                width: 330,
-                height: 55,
-                child: ElevatedButton.icon(
-                  icon: Icon(
-                    Icons.search,
-                    size: 24,
-                    color: Colors.white,
-                  ),
-                  label: Text(
-                    'Search',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color.fromARGB(255, 15, 3, 3),
-                    onPrimary: Color.fromARGB(255, 0, 0, 0),
-                    shadowColor: Color.fromARGB(68, 0, 0, 0),
-                    side: BorderSide(
-                        width: 1.5, color: Color.fromARGB(19, 0, 0, 0)),
-                    elevation: 1,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    minimumSize: Size(100, 40), //////// HERE
-                  ),
-                  onPressed: () {
+      appBar: AppBar(
+        toolbarHeight: 120,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Container(
+          width: double.infinity,
+          height: 40,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: Colors.blue)),
+          child: TextField(
+            decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.my_location_rounded,
+                  color: Colors.green,
+                ),
+                suffix: GestureDetector(
+                  onTap: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => MainPage()),
+                      MaterialPageRoute(builder: (context) => Search()),
                     );
                   },
                 ),
-              ),
-            ],
+                hintText: 'Search...',
+                border: InputBorder.none),
           ),
         ),
+        bottom: PreferredSize(
+            child: Container(
+              width: 300,
+              height: 40,
+              alignment: Alignment.topRight,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.blue)),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                    ),
+                    suffix: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Search()),
+                        );
+                      },
+                    ),
+                    hintText: 'Search...',
+                    border: InputBorder.none),
+              ),
+            ),
+            preferredSize: Size.fromHeight(2.0)),
       ),
     );
   }
